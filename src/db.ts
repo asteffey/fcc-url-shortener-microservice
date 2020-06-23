27cbf61786385bd8e6066cba33a1560857b2ce85
=Andrew Steffey <23619800+asteffey@users.noreply.github.com>
2020-06-22 22:39:18 -0400

    build: run locally

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,12 @@
 import mongoose from 'mongoose'
+import { mongoUrl } from './config'
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
 const db = mongoose.connection
+
+
 db.on('error', (err) => console.error(`MongoDB connection error: ${err}`))
-
-function close () {
-  console.log('MongoDB Closed')
-}
-
-process.on('SIGINT', close)
-process.on('SIGTERM', close)
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import app from './app'
 import { port } from './config'
+import mongoose from 'mongoose'
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on ${port}`)
 })
+
+function close () {
+  console.log('shutting down server')
+  Promise.all([server.close(), mongoose.connection.close()]).then(() => {
+    process.exit(0)
+  })
+}
+
+process.on('SIGINT', close)
+process.on('SIGTERM', close)
