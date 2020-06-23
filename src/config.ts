f9049730a75bdd20a0d1cfa0dd24ec85a14448a7
=Andrew Steffey <23619800+asteffey@users.noreply.github.com>
2020-06-22 22:39:18 -0400

    build: run locally

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const port = Number(process.env.PORT) || 3000
 export const baseUrl = process.env.BASE_URL || 'http://localhost:3000'
+export const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/url-shortener'
