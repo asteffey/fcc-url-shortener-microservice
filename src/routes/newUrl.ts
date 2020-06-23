ea89228e245fe6bb17f49a9513a581f1440f7c91
=Andrew Steffey <23619800+asteffey@users.noreply.github.com>
2020-06-22 22:57:44 -0400

    feat: return error for bad url

--- src/routes/newUrl.ts
+++ src/routes/newUrl.ts
@@ -1,3 +1,4 @@
+import urlRegex from 'url-regex'
 import { Router } from 'express'
 import { urlencoded } from 'body-parser'
 import { baseUrl } from '../config'
@@ -8,11 +9,16 @@ const newUrl = Router()
 newUrl.use(urlencoded({ extended: false }))
 
 newUrl.post('/', ({ body: { url } }, res, next) => {
-  ShortUrl.newShortUrl(url)
-    .then(({ originalUrl, shortId }) => res.json({
-      original_url: originalUrl,
-      short_url: `${baseUrl}/${shortId}`
-    })).catch(err => next(err))
+  if (urlRegex({ exact: true }).test(url)) {
+    ShortUrl.newShortUrl(url)
+      .then(({ originalUrl, shortId }) => res.json({
+        original_url: originalUrl,
+        short_url: `${baseUrl}/${shortId}`
+      })).catch(err => next(err))
+  } else {
+    res.status(400)
+    res.send(`${url} is not a valid URL`)
+  }
 })
 
 export default newUrl
